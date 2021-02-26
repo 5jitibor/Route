6656ee6a00283005d6f18b0ca9a6e425026d3fe5
TestAndroid <5jitibor@gmail.com>
2021-02-26 19:54:37 +0100

    Final Version 2.0

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  productsList:number[] =[1,2,3,4,5,6,7,8,9]
   constructor() { }
 
   ngOnInit(): void {
